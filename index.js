0f039daa8e25dfe3874071da99e2363e9ce68caa
Bin <binlin35@yahoo.com>
2021-04-08 13:24:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
+const input = document.getElementById('input');
 
-    function clickAlert() {
+function clickAlert() {
   alert('I was clicked!');
 }
-
-    input.addEventListener('click', clickAlert); 
-}
+input.addEventListener('click', clickAlert);
 
